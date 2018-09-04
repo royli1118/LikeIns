2f397a3128faf73b81190186e4a305258bfdda01
roy <royli.lihuanan@gmail.com>
2018-09-04 23:07:32 +1000

    Change some configs

--- LikeIns/Config.swift
+++ LikeIns/Config.swift
@@ -7,5 +7,5 @@
 
 import Foundation
 struct Config {
-    static var STORAGE_ROOF_REF = "gs://instagramclone-fc544.appspot.com"
+    static var STORAGE_ROOF_REF = "gs://insclone-c439d.appspot.com"
 }
